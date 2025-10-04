b2a5022c755a6e43eee343a8eb41db5b8b66909d
veena077 <veenakumawat077@gmail.com>
2025-10-04 17:04:25 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as a newer version 
+ 
+// alert( 2 + 2 ) // we are using nodejs , not browser
+
+console.log( 2 
+    + 2
+); //This is not good way to write code , we should write readable code 
+// Code readdability should be high .
+
+console.log("Veena");
+
+let name = "Veena"
+let age = 18
+let isLoggedIn = false
+let state; //undefined = value is not assigned
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => 
+//symbol => unique
+
+//object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
