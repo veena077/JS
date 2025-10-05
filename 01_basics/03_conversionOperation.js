9011b17b0c786d1f3ece5c1a91e417e1044b7f78
veena077 <veenakumawat077@gmail.com>
2025-10-05 08:41:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33abc"
+//let score = 33
+//let score = null
+//let score = undefined
+//let score = "Veena"
+//let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false 
+// "Veena" => true
+
+let someString = 33
+
+let stringNumber = String(someString)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
